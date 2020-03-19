6cb69eb490dfb16f8f5b8a01d22ec5d1c717fff8
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-03-19 16:29:47 +0900

    toggle menu

--- website/components/Navbar.vue
+++ website/components/Navbar.vue
@@ -4,6 +4,7 @@
       <a
         role="button"
         class="navbar-burger"
+        :class="{ 'is-active': showMenu }"
         aria-label="menu"
         @click="onClickBurger"
       >
@@ -42,47 +43,58 @@
         </div>
       </div>
     </nav>
-    <div class="menu">
-      <p class="menu-label">
-        General
-      </p>
-      <ul class="menu-list">
-        <li><a>Dashboard</a></li>
-        <li><a>Customers</a></li>
-      </ul>
-      <p class="menu-label">
-        Administration
-      </p>
-      <ul class="menu-list">
-        <li><a>Team Settings</a></li>
-        <li>
-          <a class="is-active">Manage Your Team</a>
-          <ul>
-            <li><a>Members</a></li>
-            <li><a>Plugins</a></li>
-            <li><a>Add a member</a></li>
-          </ul>
-        </li>
-        <li><a>Invitations</a></li>
-        <li><a>Cloud Storage Environment Settings</a></li>
-        <li><a>Authentication</a></li>
-      </ul>
-      <p class="menu-label">
-        Transactions
-      </p>
-      <ul class="menu-list">
-        <li><a>Payments</a></li>
-        <li><a>Transfers</a></li>
-        <li><a>Balance</a></li>
-      </ul>
+    <div v-show="showMenu">
+      <div class="menu-bg" @click="onClickMenuBg"></div>
+      <div class="menu">
+        <p class="menu-label">
+          General
+        </p>
+        <ul class="menu-list">
+          <li><a>Dashboard</a></li>
+          <li><a>Customers</a></li>
+        </ul>
+        <p class="menu-label">
+          Administration
+        </p>
+        <ul class="menu-list">
+          <li><a>Team Settings</a></li>
+          <li>
+            <a class="is-active">Manage Your Team</a>
+            <ul>
+              <li><a>Members</a></li>
+              <li><a>Plugins</a></li>
+              <li><a>Add a member</a></li>
+            </ul>
+          </li>
+          <li><a>Invitations</a></li>
+          <li><a>Cloud Storage Environment Settings</a></li>
+          <li><a>Authentication</a></li>
+        </ul>
+        <p class="menu-label">
+          Transactions
+        </p>
+        <ul class="menu-list">
+          <li><a>Payments</a></li>
+          <li><a>Transfers</a></li>
+          <li><a>Balance</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      showMenu: false
+    }
+  },
   methods: {
-    onClickBurger(event) {
-      event.target.classList.toggle('is-active')
+    onClickBurger() {
+      this.showMenu = true
+    },
+    onClickMenuBg() {
+      this.showMenu = false
     }
   }
 }
@@ -122,5 +134,13 @@ export default {
   z-index: 2;
   height: 100%;
   box-shadow: -2px 0px 3px rgba(0, 0, 0, 0.1);
+}
+.menu-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
 }
 </style>
