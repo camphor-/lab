34705a3e7aec20baad6a1385bab8b7eb44da82de
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-03-26 22:04:13 +0900

    langを設定した

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -8,6 +8,9 @@ export default {
    */
   head: {
     title: 'CAMPHOR- Lab',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
